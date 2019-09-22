9fa1ec07f832ec727c5f85d4627711cd7c1f7773
Administrator <trangtrunghoangphuc@gmail.com>
2019-09-23 00:53:48 +0700

    Initial Commit

---
+++ 1612521_Week1/Assets/AllyAttack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllyAttack : MonoBehaviour
+{
+    Animator anim;
+    Vector2 pos;
+    int attackHash = Animator.StringToHash("Attack");
+    public float speed = 0.1f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        pos = transform.position;
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger(attackHash);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (pos.x > -screenBounds.x)
+            {
+                pos.x -= speed;
+            }
+            transform.position = pos;
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (pos.x < screenBounds.x)
+            {
+                pos.x += speed;
+            }
+            transform.position = pos;
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
---
+++ 1612521_Week1/Assets/Enemy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Vector2 pos;
+    Animator anim;
+    int attackHash = Animator.StringToHash("Attack");
+    System.Random rand = new System.Random();
+    int cnt = 0;
+    public int frames = 50;
+    public float speed = 0.1f;
+    public int moveFrames = 20;
+    int movecnt = 0;
+    bool left = true;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        anim = GetComponent<Animator>();
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movecnt++;
+        if (left)
+        {
+            if (pos.x > 0)
+            {
+                pos.x -= speed;
+            }
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else
+        {
+            if (pos.x < screenBounds.x)
+            {
+                pos.x += speed;
+            }
+            GetComponent<SpriteRenderer>().flipX = true;
+        }
+        transform.position = pos;
+        if (movecnt > moveFrames)
+        {
+            movecnt = 0;
+            int r = rand.Next(0, 2);
+            if(r == 0)
+            {
+                left = false;
+            }
+            else
+            {
+                left = true;
+            }
+        }
+        
+        cnt++;
+        if (cnt > frames)
+        {
+            anim.SetTrigger(attackHash);
+            cnt = 0;
+        }
+    }
+}
